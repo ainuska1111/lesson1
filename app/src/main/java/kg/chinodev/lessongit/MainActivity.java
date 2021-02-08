ee6776d507a2d72eb495b7ac5dabba17da60f165
ainuska1111 <baktybekova005@gmail.com>
2021-02-08 19:04:12 +0600

    add SecondActivity

--- app/src/main/java/kg/chinodev/lessongit/MainActivity.java
+++ app/src/main/java/kg/chinodev/lessongit/MainActivity.java
@@ -2,7 +2,9 @@ package kg.chinodev.lessongit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
          myButton = findViewById(R.id.second_button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
