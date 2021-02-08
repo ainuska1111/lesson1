e39d7280dbbef96883a5da34658d8f4b35004bf4
ainuska1111 <baktybekova005@gmail.com>
2021-02-08 18:34:11 +0600

    add button

--- app/src/main/java/kg/chinodev/lessongit/MainActivity.java
+++ app/src/main/java/kg/chinodev/lessongit/MainActivity.java
@@ -3,12 +3,15 @@ package kg.chinodev.lessongit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         myButton = findViewById(R.id.second_button);
     }
 }
